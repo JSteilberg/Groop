0e867094ad2a98a19eed6c10e7f3124b62578998
Jack <steilberg.j@husky.neu.edu>
2018-03-04 11:18:26 -0500

    Added method of successive squaring

--- src/function/binary/ExponentMod.java
+++ src/function/binary/ExponentMod.java
@@ -1,4 +1,77 @@
 package function.binary;
 
-public class ExponentMod {
+import java.util.Objects;
+import java.util.function.BinaryOperator;
+
+import numtheory.Util;
+
+public class ExponentMod implements BinaryOperator<Integer> {
+  private final int modulus;
+  private final int phiModulus;
+
+  public ExponentMod(int modulus) {
+    if (modulus <= 0) {
+      throw new IllegalArgumentException("Given a negative or zero modulus");
+    }
+
+    this.modulus = modulus;
+    this.phiModulus = Util.phi(modulus);
+  }
+
+  /**
+   * Computes the power of base to the exponent mod modulus, using the method of
+   * successive squaring.
+   *
+   * @param base     Base of the exponentiation
+   * @param exponent Exponent of the exponentiation
+   * @return base<sup>exponent</sup> mod modulus
+   */
+  @Override
+  public Integer apply(Integer base, Integer exponent) {
+    Objects.requireNonNull(base);
+    Objects.requireNonNull(exponent);
+
+    // Trivial
+    if (base == 1) {
+      return 1;
+    }
+
+    // Also trivial
+    if (exponent == 1) {
+      return base % this.modulus;
+    }
+
+    // Equivalent statement. Allows one to express the inverse of a number by giving
+    // exponent -1, since it will become phi(modulus) - 1
+    if (exponent < 0) {
+      exponent %= this.phiModulus;
+    } else if (exponent == 0) {
+      return 0;
+    }
+
+    // We make a copy of the exponent for shifting, at the same time we mod by
+    // phi(modulus), since by Euler's theorem the answer will remain unchanged
+    int shiftableExponent = exponent % phiModulus;
+
+    // Accumulates the answer
+    int prodAns = 1;
+
+    // Holds the base in subsequent successive squares
+    int currExp = base % modulus;
+
+    // Used for counting the for loop -- We need to calculate every power of two exponent
+    // up to the highest one that is smaller then our given exponent.
+    // For example, if we are given an exponent of 327, we need to calculate up to 256
+    final int powOf2 = Integer.highestOneBit(exponent);
+    // Basically, we loop through each bit in the exponent, noting whether it is zero or one,
+    for (int i = 1; i <= powOf2; i *= 2) {
+      // If the lowest bit is one then we include it that power of the base in the final product
+      if (Integer.lowestOneBit(shiftableExponent) == 1) {
+        prodAns = (prodAns * currExp) % modulus;
+      }
+      currExp = (int) (Math.pow(currExp, 2)) % modulus;
+      shiftableExponent >>= 1;
+    }
+    return prodAns;
+  }
 }
--- test/function/TestBinary.java
+++ test/function/TestBinary.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
+import function.binary.ExponentMod;
 import function.binary.XPlusY;
+import numtheory.Util;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,10 +18,29 @@ public class TestBinary {
 
   @Test
   public void testLamb() {
+    Util.generateSieve(5000);
+
     Function<Double, Double> square = (x) -> (x * x);
     BiFunction<Double, Double, Double> xplusy = new XPlusY();
 
-    assertEquals("ree", 10d, xplusy.apply(7d, 3d), .01);
-    assertEquals("ree", 49d, square.apply(7d), .01);
+    assertEquals("Adding function should add 7 + 3 to get 10",
+            10d, xplusy.apply(7d, 3d), .01);
+    assertEquals("Squaring function should square 7 to get 49",
+            49d, square.apply(7d), .01);
+
+    BiFunction<Integer, Integer, Integer> expMod = new ExponentMod(853);
+
+    assertEquals("1 to any power is congruent to 1 in any modulo",
+            1, (int) expMod.apply(1, 90000));
+    assertEquals("Any base to the 1th power is congruent to base mod modulo",
+            404, (int) expMod.apply(23435, 1));
+
+    assertEquals("7^327 is congruent to 286 mod 853",
+            286, (int) expMod.apply(7, 327));
+    assertEquals("Adding a multiple of the modulus to the base or a multiple of " +
+                    "phi(modulus) to the exponent shouldn't change anything",
+            286, (int) expMod.apply(7 + 853, 327 + Util.phi(853)));
+    assertEquals("Euler's identity to find inverse. a*a^(phi(m)-1) === 1 mod(m)",
+            1, (7 * expMod.apply(7, Util.phi(853) - 1)) % 853);
   }
 }
